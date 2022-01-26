9e5bd701bdc3fd0c0eb0654be5c7379766240da9
belikx5 <i.bilicenko@gmail.com>
2022-01-26 21:39:53 +0200

    fix user creation and update

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 export default axios.create({
-  baseURL: "https://linkstagram-api.ga",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+	// baseURL: "https://linkstagram-api.ga",
+	baseURL: 'http://127.0.0.1:8000/api',
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+	},
 });
