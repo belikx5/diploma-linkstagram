232c7016ba49d3afc363472d0e417af514c6fcda
Igor Bilitsenko <i.bilicenko@gmail.com>
2022-06-26 16:02:59 +0300

    Update models.py

--- api/linkstagram/user_profile/models.py
+++ api/linkstagram/user_profile/models.py
@@ -5,9 +5,6 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #username = models.CharField(unique=True, max_length=100)
-    # first_name = models.CharField(max_length=75)
-    # last_name = models.CharField(max_length=100)
     bio = models.TextField(max_length=350, default='')
     followers_count = models.PositiveIntegerField(default=0)
     following_count = models.PositiveIntegerField(default=0)
